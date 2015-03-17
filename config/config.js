6753e077256a72cc523c1788c8ec85fbc556855b
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2015-03-17 15:01:56 +0200

    setting up

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
     env = process.env.NODE_ENV || 'development';
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT; // || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP; // || '127.0.0.1';
 
-var mongodb_connection_string = 'mongodb://127.0.0.1:27017/';
+var mongodb_connection_string; // = 'mongodb://127.0.0.1:27017/';
 //take advantage of openshift env vars when available:
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
   mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL;
