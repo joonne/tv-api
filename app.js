160303cd63c89f6f5e19bbeea4d0a5546b84ab48
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2015-03-17 14:22:18 +0200

    setting up

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express'),
   glob = require('glob'),
   mongoose = require('mongoose');
 
+console.log("is anyone here?");
+
 mongoose.connect(config.db);
 var db = mongoose.connection;
 db.on('error', function () {
