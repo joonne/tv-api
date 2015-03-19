1255ed9fd40c6235fddf2872785b2f8b0567cb5d
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2015-03-19 10:12:50 +0200

    setting up

--- config/express.js
+++ config/express.js
@@ -29,6 +29,8 @@ module.exports = function(app, config) {
   app.use(express.static(config.root + '/public'));
   app.use(methodOverride());
 
+  console.log(config.root + '/public/css');
+
   var controllers = glob.sync(config.root + '/app/controllers/*.js');
   controllers.forEach(function (controller) {
     require(controller)(app);
