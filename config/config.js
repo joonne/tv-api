210eab39abcab22222dadfeeaf9fb31174ea3a1e
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2017-10-14 18:50:26 +0300

    setting up mongodb in openshift

--- config/config.js
+++ config/config.js
@@ -8,22 +8,6 @@ const serverIpAddress = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '12
 const name = 'tv-api';
 const mongoURL = process.env.MONGO_URL || process.env.OPENSHIFT_MONGODB_DB_URL || `mongodb://127.0.0.1:27017/${name}`;
 
-// if (process.env.DATABASE_SERVICE_NAME) {
-//   const mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-//   const mongoHost = process.env[`${mongoServiceName}_SERVICE_HOST`];
-//   const mongoPort = process.env[`${mongoServiceName}_SERVICE_PORT`];
-//   const mongoDatabase = process.env[`${mongoServiceName}_DATABASE`];
-//   const mongoPassword = process.env[`${mongoServiceName}_PASSWORD`];
-//   const mongoUser = process.env[`${mongoServiceName}_USER`];
-
-//   if (mongoHost && mongoPort && mongoDatabase) {
-//     if (mongoUser && mongoPassword) {
-//       mongoURL += `${mongoUser}:${mongoPassword}@`;
-//     }
-//     mongoURL += `${mongoHost}:${mongoPort}/${mongoDatabase}`;
-//   }
-// }
-
 module.exports = {
   root: rootPath,
   app: {
